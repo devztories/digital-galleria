b237db9d284fe6597f209cadad16c2689c4a79f2
Devadathan.R <devadathan2k5@gmail.com>
2026-08-06 21:53:37 +0530

    Update build script and database settings

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -o errexit
 
 pip install -r requirements.txt
 
+
+python -c "import os; print('DATABASE_URL=', os.getenv('DATABASE_URL'))"
+
 python manage.py collectstatic --no-input
 
 python manage.py migrate
-
-python -c "import os; print('DATABASE_URL=', os.getenv('DATABASE_URL'))"
--- config/settings.py
+++ config/settings.py
@@ -158,17 +158,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ==========================================================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-        "OPTIONS": {
-            "sslmode": "require",
-        },
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 # ==========================================================
 # PASSWORD VALIDATION
