ae4f37d3af739a772b2d3d21b2a0af157e7a4701
Devadathan.R <devadathan2k5@gmail.com>
2026-08-06 21:47:12 +0530

    Fix database configuration

--- config/settings.py
+++ config/settings.py
@@ -158,13 +158,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # ==========================================================
 
 DATABASES = {
-    "default": dj_database_url.parse(
-        os.environ["DATABASE_URL"],
-        conn_max_age=600,
-        ssl_require=True,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "sslmode": "require",
+        },
+    }
 }
-
 # ==========================================================
 # PASSWORD VALIDATION
 # ==========================================================
